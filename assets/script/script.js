ac00108a94a4d1e6a207486dd5b4e4cc9556dc1e
Logan Palmer <loganpalmer95@icloud.com>
2022-01-16 21:50:23 -0500

    added start quiz functionality and first question

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,57 @@
 var myInterval = null
 var duration = 59
 var timer = duration, seconds;
+var display = document.querySelector('#time');
 
+let questions = [
+    {
+        question: "What is a function?", 
+        choice1: "A set of statements that performs a task or calculates a value.",
+        choice2: "An array",
+        choice3: "A truthy statment",
+        answer: 1
+    },
+    {
+        question: "What is a function?", 
+        choice1: "A set of statements that performs a task or calculates a value.",
+        choice2: "An array",
+        choice3: "A truthy statment",
+        answer: 1
+    },    {
+        question: "What is a function?", 
+        choice1: "A set of statements that performs a task or calculates a value.",
+        choice2: "An array",
+        choice3: "A truthy statment",
+        answer: 1
+    },    {
+        question: "What is a function?", 
+        choice1: "A set of statements that performs a task or calculates a value.",
+        choice2: "An array",
+        choice3: "A truthy statment",
+        answer: 1
+    },    {
+        question: "What is a function?", 
+        choice1: "A set of statements that performs a task or calculates a value.",
+        choice2: "An array",
+        choice3: "A truthy statment",
+        answer: 1
+    },
+]
 
+window.onload = function () {
+    var button = document.getElementById("start-button")
+    button.addEventListener('click', () => myFunction(button));
+    function myFunction(button) {
+        hideIntro();
+        startQuiz();
+        showAnswers();
+    }
+};
+
+function hideIntro() {
+    document.getElementById("start-view").style.visibility = "hidden";
+    startTimer(display);
+}
 
 function startTimer(display) {
     myInterval = setInterval(function () {
@@ -15,12 +64,22 @@ function startTimer(display) {
     }, 1000);
 }
 
-window.onload = function () {
-    display = document.querySelector('#time');
-    startTimer(display);
-};
+function startQuiz() {
+    var currentQuestion = 0 
+    var mainText = document.getElementById("main-text")
+    mainText.textContent = questions[currentQuestion].question
+    
+    var choice1 = document.getElementById("choice-1")
+    choice1.textContent = questions[currentQuestion].choice1
+    var choice2 = document.getElementById("choice-2")
+    choice2.textContent = questions[currentQuestion].choice2
+    var choice3 = document.getElementById("choice-3")
+    choice3.textContent = questions[currentQuestion].choice3
+}
 
-
+function showAnswers() {
+    document.getElementById("answer-view").style.visibility = "visible";
+}
 
 function endTimer() {
     clearInterval(myInterval)
@@ -28,7 +87,5 @@ function endTimer() {
 }
 
 function reduceTime() {
-   timer = timer - 5
+    timer = timer - 5
 }
-
-
